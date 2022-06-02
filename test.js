7e1994a5c760d6a92cc212e2f02f80b3269484da
Odediran Marius <diranmarius8@gmail.com>
2022-06-03 00:33:25 +0100

    Added new codes

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 let firstBar = document.querySelector(".pro");
 let dropDown = document.querySelector(".side-bar1");
 let icon1 = document.querySelector(".arrow1")
-let breaker = document.querySelector(".breaker");
+let breaker1 = document.querySelector(".breaker");
 let remote = document.querySelector("#remote");
 remote = true;
 firstBar.addEventListener("click", showme1)
@@ -12,14 +12,14 @@ firstBar.addEventListener("click", showme1)
             dropDown.style.transition = ".1s ease-in";
             firstBar.style.textDecoration = "Underline";
             icon1.style.transform = "rotate(180deg)";
-            breaker.style.margin = "14rem 0 0 0";
+            breaker1.style.margin = "14rem 0 0 0";
             remote =false
         } else if (remote==false){
             dropDown.style.visibility = "hidden";
             dropDown.style.transition = ".1s ease out";
             firstBar.style.textDecoration = "none";
             icon1.style.transform = "rotate(0deg)";
-            breaker.style.margin = "4rem 0 0 0";
+            breaker1.style.margin = "3rem 0 0 0";
             remote = true;
         }
     }
@@ -28,6 +28,7 @@ firstBar.addEventListener("click", showme1)
 let secondBar = document.querySelector(".com");
 let dropDown2 = document.querySelector(".side-bar2");
 let icon2 = document.querySelector(".arrow2")
+let breaker2 = document.querySelector(".breaker2");
 let remote2 = document.querySelector("#remote2");
 remote2 = true;
 secondBar.addEventListener("click", showme2)
@@ -38,12 +39,18 @@ secondBar.addEventListener("click", showme2)
             dropDown2.style.transition = ".1s ease-in";
             secondBar.style.textDecoration = "underline";
             icon2.style.transform = "rotate(180deg)";
+            breaker1.style.display = "none";
+            breaker2.style.display = "block"
+            breaker2.style.margin = "12rem 0 0 0";
             remote2 = false
         } else if (remote2==false){
             dropDown2.style.visibility = "hidden";
             dropDown2.style.transition = ".1s ease out";
             secondBar.style.textDecoration = "none";
             icon2.style.transform = "rotate(0deg)";
+            breaker1.style.display = "block";
+            breaker2.style.display = "none";
+            breaker2.style.margin = "3rem 0 0 0";
             remote2 = true;
         }
     }
